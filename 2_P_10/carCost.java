89f5c53f86d5bc708831fd80c68bbca863245ac1
Tony Lim <gunlockz@yahoo.com>
2014-08-27 23:01:17 -0700

    add Keybindings and snippetstemplate

---
+++ 2_P_10/carCost.java
@@ -0,0 +1,13 @@
+/* Name : Tony Lim
+ * Date : Aug 27, 2014
+ * Assignment : Page 72, P2.10
+ * Description : 
+ */
+
+setu
+public class carCost {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
