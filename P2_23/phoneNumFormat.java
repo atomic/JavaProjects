7c95004903514b36944e2cedf50108f37c9da735
Tony Lim <gunlockz@yahoo.com>
2014-08-29 14:05:19 -0700

    Finished P2.23

---
+++ P2_23/phoneNumFormat.java
@@ -0,0 +1,35 @@
+/* Name : Tony Lim
+ * Date : Aug 29, 2014
+ * Assignment : Page 75, P2.23
+ * Description : Formatting phone numberoC
+ */
+import java.util.Scanner;
+
+
+public class phoneNumFormat {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the unformatted phone number (10-digit) : ");
+		String numInput = in.next();
+		System.out.printf("Formatted number : %s", processedNum(numInput));
+	}
+
+	/**
+	 * This function takes in unformatted phone number and return the phone number 
+	 * 	in the format of (xxx) xxx-xxxx, extra number beyond 10 digits will be added 
+	 * 	at the end of the numbers
+	 * @param inString
+	 * @return
+	 */
+	public static String processedNum(String inString) {
+		String newString = "("; 
+		newString += inString.substring(0,3);
+		newString += ") ";
+		newString += inString.substring(3,6);
+		newString += "-";
+		newString += inString.substring(6);
+		return newString;
+	}
+
+}
