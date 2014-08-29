9a6dea0b8c742c90c786a0c09a4f828cddd880a4
Tony Lim <gunlockz@yahoo.com>
2014-08-28 22:44:32 -0700

    Finished with P2_10

--- P2_10/carCost.java
+++ P2_10/carCost.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.Scanner;
 
 /* Name : Tony Lim
@@ -7,15 +9,28 @@ import java.util.Scanner;
  */
 
 public class carCost {
-	public static double resaleValueInFiveY(double initial, double miles,
-										  double pricePG, double efficiencyMPG, double depreciatedBy) {
-		double totalCost = initial; //This should be 0 ? since the initial cost of the car does not matter
-		double galonUsed = miles/efficiencyMPG * 5;
-		double totalGasPrice = galonUsed*pricePG;
-		return totalGasPrice + depreciatedBy;
-	}
+
 	public static void main(String[] args) {
-		Scanner System.out,print(""); = new Scanner(System.in);
+		/**
+		 * From "http://www.kbb.com/new-cars/total-cost-of-ownership/",
+		 * the average depreciation value is 44%, so let's make this a constant.
+		 */
+		final double AVG_DEPRECIATE_VAL = 0.44;
+		
+		
+		// In a separate text file, i will provide an example of info about a Hybrid car and non-hybrid car
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("carExample.txt"));
+			String line = null;
+			while ((line = br.readLine()) != null)
+				System.out.println(line);
+			br.close();
+		} catch (Exception e) {
+			System.err.println("The file does not exist.");
+		}
+
+
+		Scanner in = new Scanner(System.in);
 		System.out.printf("Car cost : ");
 		double carCost = in.nextDouble();
 		System.out.printf("Estimated miles driven per year : ");
@@ -24,8 +39,18 @@ public class carCost {
 		double gasPrice = in.nextDouble();
 		System.out.printf("Efficiency in miles per galon : ");
 		double efficiency = in.nextDouble();
-		System.out.printf("Estimated resale value after 5 years: ");
-		double resaleInFiveYear = in.nextDouble();
-		System.out.print("awdawdawd");
+		
+		
+		double deprecVal = AVG_DEPRECIATE_VAL*carCost;
+		System.out.printf("Estimated resale value after 5 years (calculated automatically : %.2f", carCost - deprecVal);
+
+		double galonUsed = miles/efficiency* 5;
+		double totalGasPrice = galonUsed*gasPrice;
+		
+		System.out.println();
+		System.out.printf("The cost of the car in 5 years is $ %.2f", totalGasPrice + carCost + deprecVal);
+		
+		in.close();
 	}
+
 }
