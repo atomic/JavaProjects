7cf00a7221f815bc8e20736dbfde38b40fdad56c
Tony Lim <gunlockz@yahoo.com>
2014-09-03 21:18:19 -0700

    Finisehd P4_20

---
+++ P4_20/asteriskSquare.java
@@ -0,0 +1,116 @@
+import java.util.Scanner;
+
+/* Name : Tony Lim
+ * Date : Sep 3, 2014
+ * Assignment : P4.20
+ * Description : The program will print a display of solid square and hollow square
+ */
+
+
+class asteriskSquare {
+
+    public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	int input;
+	String option;
+	boolean done = false;
+	do {
+	    System.out.print("Input the integer : ");
+	    input = in.nextInt();
+	    
+	    //first, print the first 2 block of the squares
+	    printRepeatedString("*", input); 
+	    System.out.print(" ");
+	    printRepeatedString("*", input);
+	    System.out.println();
+	    //then, print the next n-2 solid and hollow square
+	    for (int i = 0; i < input - 2; i++) {
+		printRepeatedString("*", input);
+		System.out.print(" *");
+		printRepeatedString(" ", input - 2);
+		System.out.print("*\n");
+	    }
+	    printRepeatedString("*", input); 
+	    System.out.print(" ");
+	    printRepeatedString("*", input);
+	    System.out.println();
+
+	    System.out.print("are you done (y/n) : ");
+	    option = in.next();
+	    done = (option.equals("y") ? true : false);
+	} while (!done);
+	in.close();
+	System.out.print("Cosing Program");
+    }
+    
+
+    static void printRepeatedString(String str, int n) {
+	String toPrint = "";
+	for (int i = 0; i < n; i++)
+	    toPrint += str;
+	System.out.print(toPrint);
+    }
+
+}
+
+/**  	TEST CASES
+
+
+Input the integer : 5
+***** *****
+***** *   *
+***** *   *
+***** *   *
+***** *****
+are you done (y/n) : n
+Input the integer : 4
+**** ****
+**** *  *
+**** *  *
+**** ****
+are you done (y/n) : n
+Input the integer : 3
+*** ***
+*** * *
+*** ***
+are you done (y/n) : n
+Input the integer : 2
+** **
+** **
+are you done (y/n) : n
+Input the integer : 1
+* *
+* *
+are you done (y/n) : n
+Input the integer : 10
+********** **********
+********** *        *
+********** *        *
+********** *        *
+********** *        *
+********** *        *
+********** *        *
+********** *        *
+********** *        *
+********** **********
+are you done (y/n) : n
+Input the integer : 15
+*************** ***************
+*************** *             *
+*************** *             *
+*************** *             *
+*************** *             *
+*************** *             *
+*************** *             *
+*************** *             *
+*************** *             *
+*************** *             *
+*************** *             *
+*************** *             *
+*************** *             *
+*************** *             *
+*************** ***************
+are you done (y/n) : y
+
+
+*/
