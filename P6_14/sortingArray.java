f2d4549c74ad6a845bf7b268f553c40bcb7693a7
Tony Lim[Ubuntu] <gunlockz@yahoo.com>
2014-09-07 22:58:49 -0700

    P6_14

---
+++ P6_14/sortingArray.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+/* Name : Tony Lim
+ * Date : Sep 7, 2014
+ * Assignment : P6.14
+ * Description : the program demonstrates how to generate random numbers and sort and arrays
+ */
+
+
+public class sortingArray {
+	public static void main(String[] args) {
+		ArrayList<int> randomValues = new ArrayList<>();
+		
+	}
+}
