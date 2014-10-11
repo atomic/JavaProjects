3ed4ecc8768fff5a5824243355586c06c2bccdd7
Tony Lim <atomictheorist@gmail.com>
2014-10-11 11:37:39 -0700

    Start P9_19

---
+++ P9_19/testFormattable.java
@@ -0,0 +1,15 @@
+/* Name : Tony Lim
+ * Date : Oct 11, 2014
+ * Assignment : P9.19
+ * Description : Implement formattable
+ */
+
+
+public class testFormattable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
