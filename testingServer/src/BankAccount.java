7f7d09c3f96e95f996a2f80f6c357f06914e95c0
Tony Lim <atomictheorist@gmail.com>
2014-11-25 11:26:24 -0800

    Remake testingServer

---
+++ testingServer/src/BankAccount.java
@@ -0,0 +1,30 @@
+import javafx.scene.AmbientLight;
+
+
+public class BankAccount
+{
+    private double balance;
+    private int accountNumber;
+    private static int lastAssignedNumber = 1000;
+
+    public BankAccount()
+    {
+        lastAssignedNumber++;
+        accountNumber = lastAssignedNumber;
+    }
+
+    public void deposit(double amount)
+    {
+        balance += amount;
+    }
+
+    public void withdraw(double amount)
+    {
+        balance -= amount;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+}
